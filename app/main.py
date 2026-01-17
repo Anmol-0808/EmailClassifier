e25bbe5490516e69502c5f27a515b2c2a84b880a
Anmol Srivastava <158839026+ANMOlSRI08@users.noreply.github.com>
2026-01-18 02:10:01 +0530

    Added Patch and Delete routes

--- app/main.py
+++ app/main.py
@@ -17,7 +17,10 @@ Base.metadata.create_all(bind=engine)
 
 class EmailRequest(BaseModel):
     email: EmailStr
+    email_type:str
 
+class EmailUpdate(BaseModel):
+    email_type:str
 
 def get_db():
     db = SessionLocal()
@@ -30,7 +33,8 @@ def get_db():
 @app.post("/emails", status_code=status.HTTP_201_CREATED)
 def save_email(request: EmailRequest, db: Session = Depends(get_db)):
     try:
-        new_email = Email(email=request.email)
+        new_email = Email(email=request.email,
+                          email_type=request.email_type)
         db.add(new_email)
         db.commit()
         db.refresh(new_email)
@@ -38,6 +42,7 @@ def save_email(request: EmailRequest, db: Session = Depends(get_db)):
         return {
             "message": "Email saved successfully",
             "email": new_email.email,
+            "email_type":new_email.email_type,
             "created_at": new_email.created_at
         }
 
@@ -45,16 +50,68 @@ def save_email(request: EmailRequest, db: Session = Depends(get_db)):
         db.rollback()
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already exists"
+            detail="Invalid Email or Email already exists"
         )
 
 
 @app.get("/emails",response_model=List[EmailResponse])
-def get_emails(limit:int=10,db:Session=Depends(get_db)):
+def get_emails(limit:int=10,offset:int =0,db:Session=Depends(get_db)):
     emails=(
         db.query(Email)
+        .filter(Email.is_active==True)
         .order_by(Email.created_at.desc())
+        .offset(offset)
         .limit(limit)
         .all()
     )
     return emails
+
+@app.patch("/emails/{email_id}",response_model=EmailResponse)
+def update_email_type(
+    email_id:int,
+    request: EmailUpdate,
+    db:Session=Depends(get_db)
+):
+    email=(
+        db.query(Email)
+        .filter(Email.id==email_id,Email.is_active==True)
+        .first()
+    )
+    if not email:
+        raise HTTPException(
+            status_code=404,
+            detail="Email Not Found"
+        )
+    try:
+        email.email_type=request.email_type
+        db.commit()
+        db.refresh(email)
+        return email
+    
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid Email type or Duplicate email"
+        )
+    
+@app.delete("/emails/{email_id}",status_code=204)
+def delete_email(
+    email_id:int,
+    db: Session=Depends(get_db)
+):  
+    email=(
+        db.query(Email)
+        .filter(
+            Email.id==email_id,Email.is_active==True
+        )
+        .first()
+    )
+
+    if not email:
+        raise HTTPException(
+            status_code=404,
+            detail="Email Not Found"
+        )
+    email.is_active=False
+    db.commit()
--- app/models/email.py
+++ app/models/email.py
@@ -1,13 +1,45 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    CheckConstraint,
+    UniqueConstraint,
+    func,
+    text
+)
 from app.database import Base
+
 
 class Email(Base):
     __tablename__ = "emails"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, nullable=False)
+
+    is_active = Column(
+        Boolean,
+        nullable=False,
+        server_default=text("TRUE")
+    )
+
+    email_type = Column(String, nullable=False)
+
     created_at = Column(
         DateTime,
         nullable=False,
         server_default=func.now()
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            "email_type IN ('newsletter', 'support', 'marketing')",
+            name="email_type_check"
+        ),
+        UniqueConstraint(
+            "email",
+            "email_type",
+            name="email_email_type_unique"
+        ),
     )
