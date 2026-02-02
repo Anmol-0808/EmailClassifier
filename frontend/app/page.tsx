db53b880edf87e7f6c09fbe95c3c28d62f97c760
Anmol Srivastava <158839026+ANMOlSRI08@users.noreply.github.com>
2026-02-02 13:22:27 +0530

    Final commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,5 @@
-import Link from "next/link"
+import { redirect } from "next/navigation";
 
-export default function Home(){
-  return(
-    <main style={{padding:"40px"}}>
-      <h1>MailMind</h1>
-      <Link href="/login">
-      Go to Login
-      </Link>
-    </main>
-  )
+export default function Home() {
+  redirect("/login");
 }
